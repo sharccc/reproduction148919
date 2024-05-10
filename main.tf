0979291ae7f75e167532105d80aede1ac55e162b
Sharon Cole <sharon.cole@hashicorp.com>
2024-05-10 11:45:13 +1000

    sample

---
+++ main.tf
@@ -0,0 +1,69 @@
+terraform {}
+
+provider "time" {}
+
+variable "line1" {
+  description = "this reproduction is testing uploading a module to the registry with several input vairables and each has very long description and checking whether the display of the variables on the module page is truncated and this is variable 1"
+}
+
+variable "line2" {
+  description = "this reproduction is testing uploading a module to the registry with several input vairables and each has very long description and checking whether the display of the variables on the module page is truncated and this is variable 2"
+}
+
+variable "line3" {
+  description = "this reproduction is testing uploading a module to the registry with several input vairables and each has very long description and checking whether the display of the variables on the module page is truncated and this is variable 3"
+}
+
+variable "line4" {
+  description = "this reproduction is testing uploading a module to the registry with several input vairables and each has very long description and checking whether the display of the variables on the module page is truncated and this is variable 4"
+}
+
+variable "line5" {
+  description = "this reproduction is testing uploading a module to the registry with several input vairables and each has very long description and checking whether the display of the variables on the module page is truncated and this is variable 5"
+}
+
+variable "line6" {
+  description = "this reproduction is testing uploading a module to the registry with several input vairables and each has very long description and checking whether the display of the variables on the module page is truncated and this is variable 6"
+}
+
+variable "line7" {
+  description = "this reproduction is testing uploading a module to the registry with several input vairables and each has very long description and checking whether the display of the variables on the module page is truncated and this is variable 7"
+}
+
+variable "line8" {
+  description = "this reproduction is testing uploading a module to the registry with several input vairables and each has very long description and checking whether the display of the variables on the module page is truncated and this is variable 8"
+}
+
+variable "line9" {
+  description = "this reproduction is testing uploading a module to the registry with several input vairables and each has very long description and checking whether the display of the variables on the module page is truncated and this is variable 9"
+}
+
+variable "line10" {
+  description = "this reproduction is testing uploading a module to the registry with several input vairables and each has very long description and checking whether the display of the variables on the module page is truncated and this is variable 10"
+}
+
+variable "line11" {
+  description = "this reproduction is testing uploading a module to the registry with several input vairables and each has very long description and checking whether the display of the variables on the module page is truncated and this is variable 11"
+}
+
+variable "line12" {
+  description = "this reproduction is testing uploading a module to the registry with several input vairables and each has very long description and checking whether the display of the variables on the module page is truncated and this is variable 12"
+}
+
+variable "line13" {
+  description = "this reproduction is testing uploading a module to the registry with several input vairables and each has very long description and checking whether the display of the variables on the module page is truncated and this is variable 13"
+}
+
+variable "line14" {
+  description = "this reproduction is testing uploading a module to the registry with several input vairables and each has very long description and checking whether the display of the variables on the module page is truncated and this is variable 14"
+}
+
+variable "line15" {
+  description = "this reproduction is testing uploading a module to the registry with several input vairables and each has very long description and checking whether the display of the variables on the module page is truncated and this is variable 15"
+}
+
+variable "line16" {
+  description = "this reproduction is testing uploading a module to the registry with several input vairables and each has very long description and checking whether the display of the variables on the module page is truncated and this is variable 16"
+}
+
+resource "time_static" "sample" {}
